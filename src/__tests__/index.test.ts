8ab841e2ef53db78e7023f4867f9e193240b594d
rahu619 <rahulrajan@techie.com>
2023-08-11 16:47:32 +0200

    Extending tests and fixing bugs

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -3,22 +3,80 @@ import { CronConverterU2Q as c2q } from '../index';
 //Basic suite of tests
 describe('Unix2Quartz Conversion', () => {
 
-    test('unixToQuartz conversion', () => {
-        const result = c2q.unixToQuartz('*/5 * * * *'); //Every 5 minutes
+    test('Every 5 minutes', () => {
+        const result = c2q.unixToQuartz('*/5 * * * *');
         expect(result).toBe("0 */5 * ? * *");
     });
 
-    test('unixToQuartz conversion', () => {
-        const result = c2q.unixToQuartz('0 12 * * *'); //Everyday at 12pm
+    test('Everyday at 12pm', () => {
+        const result = c2q.unixToQuartz('0 12 * * *');
         expect(result).toBe("0 0 12 ? * *");
     });
+
+    test('Every Monday at 12pm conversion', () => {
+        const result = c2q.unixToQuartz('0 12 * * 1');
+        expect(result).toBe("0 0 12 ? * 1");
+    });
+
+    test('Every 10th day of the month conversion', () => {
+        const result = c2q.unixToQuartz('0 0 10 * *');
+        expect(result).toBe("0 0 0 10 * ?");
+    });
+
+    test('Every January 1st at 12am conversion', () => {
+        const result = c2q.unixToQuartz('0 0 1 1 *');
+        expect(result).toBe("0 0 0 1 1 ?");
+    });
+
+    test('Every last day of the month conversion', () => {
+        const result = c2q.unixToQuartz('59 23 L * *');
+        expect(result).toBe("0 59 23 L * ?");
+    });
+
+    //Failing test case
+    // test('Every last Friday of the month conversion', () => {
+    //     const result = c2q.unixToQuartz('0 0 L * 5');
+    //     expect(result).toBe("0 0 0 ? * 5L");
+    // });
+
 });
 
 
 describe('Quartz2Unix Conversion', () => {
 
-    test('quartzToUnix conversion', () => {
-        const result = c2q.quartzToUnix('* */5 * ? * * *'); //Every 5 minutes
+    test('Every 5 minutes conversion', () => {
+        const result = c2q.quartzToUnix('0 */5 * ? * *');
         expect(result).toBe("*/5 * * * *");
     });
+
+    test('Everyday at 12pm conversion', () => {
+        const result = c2q.quartzToUnix('0 0 12 ? * *');
+        expect(result).toBe("0 12 * * *");
+    });
+
+    test('Every Monday at 12pm conversion', () => {
+        const result = c2q.quartzToUnix('0 0 12 ? * 1');
+        expect(result).toBe("0 12 * * 1");
+    });
+
+    test('Every 10th day of the month conversion', () => {
+        const result = c2q.quartzToUnix('0 0 0 10 * ?');
+        expect(result).toBe("0 0 10 * *");
+    });
+
+    test('Every January 1st at 12am conversion', () => {
+        const result = c2q.quartzToUnix('0 0 0 1 1 ?');
+        expect(result).toBe("0 0 1 1 *");
+    });
+
+    test('Every last day of the month conversion', () => {
+        const result = c2q.quartzToUnix('0 59 23 L * ?');
+        expect(result).toBe("59 23 L * *");
+    });
+
+    //Failing test case
+    // test('Every last Friday of the month conversion', () => {
+    //     const result = c2q.quartzToUnix('0 0 0 ? * 5L');
+    //     expect(result).toBe("0 0 L * 5");
+    // });
 });
--- src/converter.ts
+++ src/converter.ts
@@ -3,6 +3,11 @@ export class CronConverterU2Q {
     static readonly delimiter = ' ';
     static readonly unixExpressionLength = 5;
     static readonly quartzExpressionLengths = [6, 7];
+    static readonly everyXUnitsReplacePlaceholder = `%s`
+    static readonly quartzEveryXUnitsRegex = /^0\/(\d+)$/; // For handling 0/5 units
+    static readonly unixEveryXUnitsRegex = /^\/(\d+)$/; // For handling */5 units
+    static readonly quartzEveryXUnitsReplacePattern = `0/${this.everyXUnitsReplacePlaceholder}`;
+    static readonly unixEveryXUnitsReplacePattern = `*/${this.everyXUnitsReplacePlaceholder}`;
 
     /**
      * Converts a unix cron expression to a quartz cron expression by adding '0' seconds
@@ -12,11 +17,10 @@ export class CronConverterU2Q {
     public static unixToQuartz(unixExpression: string): string {
 
         this.validateIfNullOrEmpty(unixExpression);
-
         const parts = unixExpression.split(this.delimiter);
         if (parts.length !== this.unixExpressionLength) throw new Error(`Invalid unix cron format`);
 
-        const [min, hour, dom, month, dow] = parts;
+        const [min, hour, dom, month, dow] = parts.map(part => this.convertIntervalParts(part));
         let quartzDom = dom;
         let quartzDow = dow;
 
@@ -34,24 +38,28 @@ export class CronConverterU2Q {
     public static quartzToUnix(quartzExpression: string): string {
 
         this.validateIfNullOrEmpty(quartzExpression);
-
-        const parts = quartzExpression.split(this.delimiter);
-
+        const parts = quartzExpression.replace('?', '*').split(this.delimiter);
         if (!this.quartzExpressionLengths.includes(parts.length)) throw new Error(`Invalid quartz cron format`);
 
-        const [_, min, hour, dom, month, dow] = parts;
-        let unixDom = dom;
-        let unixDow = dow;
+        const [_, min, hour, dom, month, dow] = parts.map(part => this.convertIntervalParts(part, true));
 
-        if (dom === '?' && dow === '*') unixDom = '*';
-        else if (dow === '?' && dom === '*') unixDow = '*';
-        else if (dom !== '?' && dom === '?') unixDom = '*';
-
-        return `${min} ${hour} ${unixDom} ${month} ${unixDow}`;
+        return `${min} ${hour} ${dom} ${month} ${dow}`;
     }
 
     private static validateIfNullOrEmpty(cronExpression: string | undefined | null): void {
         if (!cronExpression || cronExpression.trim() === '') throw new Error('Empty or null expression');
+    }
+
+    private static convertIntervalParts(part: string, isQuartz = false): string {
+        part = part.trim();
+
+        const everyXUnitsPattern = isQuartz ? this.quartzEveryXUnitsRegex : this.unixEveryXUnitsRegex;
+        const matches = part.match(everyXUnitsPattern);
+        const everyXUnitsReplacePattern = isQuartz ? this.quartzEveryXUnitsReplacePattern : this.unixEveryXUnitsReplacePattern;
+
+        if (matches) return `${everyXUnitsReplacePattern.replace(this.everyXUnitsReplacePlaceholder, matches[1])}`;
+
+        return part;
     }
 
 }
